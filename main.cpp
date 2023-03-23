2c83125efabbf619b3a306ce3e5ef236e825e1a7
Jakub Nowak <jakub.nowak@zst.kolbuszowa.pl>
2023-03-23 12:08:49 +0100

    switch case

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream> // wykonaj program switch case jak dla wcześniejszego programu if else
+using namespace std;
+int alfabet;
+int main() {
+    cout << "wstaw liczbe od 1 do 26: ";
+    cin >> alfabet;
+    switch (alfabet) {
+        case 1:
+            cout << "a ";
+            break;
+        case 2:
+            cout << "b ";
+            break;
+        case 3:
+            cout << "c ";
+            break;
+        case 4:
+            cout << "d ";
+            break;
+        case 5:
+            cout << "e ";
+            break;
+        case 6:
+            cout << "f ";
+            break;
+        case 7:
+            cout << "g ";
+            break;
+        case 8:
+            cout << "h ";
+            break;
+        case 9:
+            cout << "i ";
+            break;
+        case 10:
+            cout << "j ";
+            break;
+        case 11:
+            cout << "k ";
+            break;
+        case 12:
+            cout << "l ";
+            break;
+        case 13:
+            cout << "m ";
+            break;
+        case 14:
+            cout << "n ";
+            break;
+        case 15:
+            cout << "o ";
+            break;
+        case 16:
+            cout << "p ";
+            break;
+        case 17:
+            cout << "q ";
+            break;
+        case 18:
+            cout << "r ";
+            break;
+        case 19:
+            cout << "s ";
+            break;
+        case 20:
+            cout << "t ";
+            break;
+        case 21:
+            cout << "u ";
+            break;
+        case 22:
+            cout << "v ";
+            break;
+        case 23:
+            cout << "w ";
+            break;
+        case 24:
+            cout << "x ";
+            break;
+        case 25:
+            cout << "y ";
+            break;
+        case 26:
+            cout << "z ";
+            break;
+    }
+    return 0;
+}
